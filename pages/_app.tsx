e850d80603071361f58405840705e9bb993193f5
Joe Gilmore <joe@joemore.com>
2023-02-07 08:05:03 -0300

    Renamed app.tsx Endpoints

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,9 +26,9 @@ const authHeader = async () => {
 
 AmplifyAuth.configure({
   Auth: {
-    region: process.env.NEXT_PUBLIC_COGNITO_REGION,
-    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USERPOOLID,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USERPOOLWEBCLIENTID,
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_WEB_CLIENT_ID,
   },
 });
 
